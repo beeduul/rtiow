6d88f6a691f3c53566fa8aeabd23aa3a777a4a5f
Jeremy Biddle <biddle@adobe.com>
2018-01-28 01:50:09 -0800

    camera fov & aspect ratio

--- camera.hpp
+++ camera.hpp
@@ -10,9 +10,21 @@ public:
         vertical = vec3(0, 2, 0);
         origin = vec3(0, 0, 0);
     }
-    
-    ray get_ray(float u, float v) const { return ray(origin, lower_left_corner + u*horizontal + v*vertical - origin); }
-    
+
+    camera(float vfov, float aspect) { // vfov is top to bottom, in degrees
+        float theta = vfov * M_PI / 180;
+        float half_height = tan(theta / 2);
+        float half_width = aspect * half_height;
+        lower_left_corner = vec3(-half_width, -half_height, -1);
+        horizontal = vec3(2 * half_width, 0, 0);
+        vertical = vec3(0, 2 * half_height, 0);
+        origin = vec3(0, 0, 0);
+    }
+
+    ray get_ray(float u, float v) const {
+        return ray(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+    }
+
     vec3 origin;
     vec3 lower_left_corner;
     vec3 horizontal;
